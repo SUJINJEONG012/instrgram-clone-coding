23385a2f55a7e56ebef0b3268a155509b0ee83d4
SUJINJEONG012 <peekaboo32@naver.com>
2023-03-04 14:16:46 +0900

    Spring Boot JPA로 인스타그램클론코딩 공부

---
+++ src/main/java/cloneinstrgramstudy/InstrgramApplication.java
@@ -0,0 +1,13 @@
+package cloneinstrgramstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstrgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstrgramApplication.class, args);
+	}
+
+}
